250c3d7e05be2064076c364ec647e10cd4f500ff
zmiiovskyi <zmiiovskyi.m@gmail.com>
2026-01-22 11:02:52 +0200

    update

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask import render_template
 
@@ -7,3 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
